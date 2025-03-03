48915758ee3ba0b6fd61f6b6112bad19a4adc526
gui55gui <guilherme.gmachado95@gmail.com>
2025-03-03 20:43:21 -0300

    Insert of User

--- src/main/java/com/guimachado/workshopmongo/resources/UserResource.java
+++ src/main/java/com/guimachado/workshopmongo/resources/UserResource.java
@@ -6,7 +6,9 @@ import com.guimachado.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,6 +30,13 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+        User obj = service.insert(service.fromDTO(objDto));
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/com/guimachado/workshopmongo/services/UserService.java
+++ src/main/java/com/guimachado/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.guimachado.workshopmongo.services;
 
 import com.guimachado.workshopmongo.domain.User;
+import com.guimachado.workshopmongo.dto.UserDTO;
 import com.guimachado.workshopmongo.repository.UserRepository;
 import com.guimachado.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = rep.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return rep.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
