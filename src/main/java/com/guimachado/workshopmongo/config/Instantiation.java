f6ec76dc93f7602271618a73854b990ec913c256
gui55gui <guilherme.gmachado95@gmail.com>
2025-03-03 19:29:10 -0300

    Instantiation operation of DB

---
+++ src/main/java/com/guimachado/workshopmongo/config/Instantiation.java
@@ -0,0 +1,27 @@
+package com.guimachado.workshopmongo.config;
+
+import com.guimachado.workshopmongo.domain.User;
+import com.guimachado.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
