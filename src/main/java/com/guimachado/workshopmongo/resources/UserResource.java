7ccb0fafc7fd3dffc98f4dfcf2a8a9e52b7872a8
gui55gui <guilherme.gmachado95@gmail.com>
2025-03-03 20:06:34 -0300

    Get User by id and exception handler

--- src/main/java/com/guimachado/workshopmongo/resources/UserResource.java
+++ src/main/java/com/guimachado/workshopmongo/resources/UserResource.java
@@ -5,9 +5,7 @@ import com.guimachado.workshopmongo.dto.UserDTO;
 import com.guimachado.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,6 +22,12 @@ public class UserResource {
         List<User> users = service.findAll();
         List<UserDTO> usersDTO = users.stream().map(UserDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(usersDTO);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 
 }
--- src/main/java/com/guimachado/workshopmongo/services/UserService.java
+++ src/main/java/com/guimachado/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.guimachado.workshopmongo.services;
 
 import com.guimachado.workshopmongo.domain.User;
 import com.guimachado.workshopmongo.repository.UserRepository;
+import com.guimachado.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return rep.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = rep.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
