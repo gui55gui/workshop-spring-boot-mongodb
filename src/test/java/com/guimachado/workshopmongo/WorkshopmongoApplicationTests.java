51caf2b8c6abb37e403470a2a98d29ce9bfcf72b
gui55gui <guilherme.gmachado95@gmail.com>
2025-03-03 16:04:07 -0300

    First commit, project start

---
+++ src/test/java/com/guimachado/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guimachado.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
