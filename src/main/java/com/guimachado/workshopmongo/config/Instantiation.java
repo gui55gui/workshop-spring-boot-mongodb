cfd06ec5f91fb33a58c9715a53ce1ebb1ad69737
gui55gui <guilherme.gmachado95@gmail.com>
2025-03-04 12:17:11 -0300

    Users reference to Posts(lazy)

--- src/main/java/com/guimachado/workshopmongo/config/Instantiation.java
+++ src/main/java/com/guimachado/workshopmongo/config/Instantiation.java
@@ -40,5 +40,8 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, Date.from(LocalDate.parse("25/06/2025", formatter).atStartOfDay(ZoneId.systemDefault()).toInstant()), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
