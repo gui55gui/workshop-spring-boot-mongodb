5bb4c5f3ff35997702556d0f56be4068b03d5b76
gui55gui <guilherme.gmachado95@gmail.com>
2025-03-04 12:09:28 -0300

    Data projection of AuthorDTO

--- src/main/java/com/guimachado/workshopmongo/config/Instantiation.java
+++ src/main/java/com/guimachado/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.guimachado.workshopmongo.config;
 
 import com.guimachado.workshopmongo.domain.Post;
 import com.guimachado.workshopmongo.domain.User;
+import com.guimachado.workshopmongo.dto.AuthorDTO;
 import com.guimachado.workshopmongo.repository.PostRepository;
 import com.guimachado.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,12 +32,13 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        Post post1 = new Post(null, Date.from(LocalDate.parse("15/03/2025", formatter).atStartOfDay(ZoneId.systemDefault()).toInstant()), "Partiu Viagem", "Vou viajar para São Paulo, partiu abraços!", maria);
-        Post post2 = new Post(null, Date.from(LocalDate.parse("25/06/2025", formatter).atStartOfDay(ZoneId.systemDefault()).toInstant()), "Bom dia!", "Acordei feliz hoje!", maria);
+        Post post1 = new Post(null, Date.from(LocalDate.parse("15/03/2025", formatter).atStartOfDay(ZoneId.systemDefault()).toInstant()), "Partiu Viagem", "Vou viajar para São Paulo, partiu abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, Date.from(LocalDate.parse("25/06/2025", formatter).atStartOfDay(ZoneId.systemDefault()).toInstant()), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
 
-        userRepository.saveAll(Arrays.asList(maria, alex, bob));
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
