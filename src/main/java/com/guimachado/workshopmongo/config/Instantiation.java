4cb033482673ef8124d571f7b1b4bb8c3e72993d
gui55gui <guilherme.gmachado95@gmail.com>
2025-03-04 11:57:47 -0300

    Post entity created with user nested

--- src/main/java/com/guimachado/workshopmongo/config/Instantiation.java
+++ src/main/java/com/guimachado/workshopmongo/config/Instantiation.java
@@ -1,27 +1,42 @@
 package com.guimachado.workshopmongo.config;
 
+import com.guimachado.workshopmongo.domain.Post;
 import com.guimachado.workshopmongo.domain.User;
+import com.guimachado.workshopmongo.repository.PostRepository;
 import com.guimachado.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Date;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    PostRepository postRepository;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        Post post1 = new Post(null, Date.from(LocalDate.parse("15/03/2025", formatter).atStartOfDay(ZoneId.systemDefault()).toInstant()), "Partiu Viagem", "Vou viajar para São Paulo, partiu abraços!", maria);
+        Post post2 = new Post(null, Date.from(LocalDate.parse("25/06/2025", formatter).atStartOfDay(ZoneId.systemDefault()).toInstant()), "Bom dia!", "Acordei feliz hoje!", maria);
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ src/main/java/com/guimachado/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.guimachado.workshopmongo.repository;
+
+import com.guimachado.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
